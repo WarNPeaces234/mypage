7906f56b807dda790a112031c0b55758704e6b46
WarNPeaces234 <alequinjoshua@gmail.com>
2015-02-10 09:10:07 -0500

    Update guts.js

--- js/guts.js
+++ js/guts.js
@@ -1,6 +1,24 @@
-function divide(x, y) {
+function divide(x, y); {
 	return x/y;
 }
-function multiply(x, y) {
- return x;
+function multiply(x, y); {
+ return x*y;
+}
+function add(x, y); {
+	return x+y;
+}
+function subtract(x, y); {
+	return x-y;
+}
+function generateRandomNumber(x); {
+	 Math.random();	
+}
+function power(x, y); {
+	Math.pow(x,y);
+}
+function square_root(x); {
+	Math.sqrt(x);
+}
+function logBase10(x); {
+	Math.LOG10E;
 }
